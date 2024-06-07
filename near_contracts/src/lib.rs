c49b5aaf9f734e44d3f29de8180834ca0a222ac6
kdonthi <kaushikdr@gmail.com>
2024-06-07 01:00:49 +0100

    bring rust contract

---
+++ near_contracts/src/lib.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+// Find all our documentation at https://docs.near.org
+use hex::decode;
+use near_sdk::{env, ext_contract, near, require, Gas, NearToken, Promise, AccountId};
+
+const MPC_CONTRACT_ACCOUNT_ID: &str = "v2.multichain-mpc.testnet";
+const MINUTE: u64 = 60 * 1_000_000_000;
+
+// interface for cross contract call to mpc contract
+#[ext_contract(mpc)]
+trait MPC {
+    fn sign(&self, payload: [u8; 32], path: String, key_version: u32) -> Promise;
+}
+
+// automatically init the contract
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            users_to_time: HashMap::new()
+        }
+    }
+}
+
+#[near(contract_state)]
+pub struct Contract {
+    users_to_time: HashMap<AccountId, u64>
+}
+
+#[near]
+impl Contract {
+    // proxy to call MPC_CONTRACT_ACCOUNT_ID method sign if COST is deposited
+    #[payable]
+    pub fn sign(&mut self, rlp_payload: [u8; 32], path: String, key_version: u32) -> Promise {
+        let sender = env::predecessor_account_id();
+        let result = self.users_to_time.get(&sender);
+
+        match result {
+            None => {}
+            Some(ts) => {
+                let current_time_rewinded_minute = env::block_timestamp() - MINUTE;
+                require!(ts < &current_time_rewinded_minute, "Cannot make more than one call per minute");
+            }
+        }
+
+        self.users_to_time.insert(sender.clone(), env::block_timestamp());
+
+        // hash and reverse rlp encoded payload
+        let payload: [u8; 32] = env::keccak256_array(&decode(rlp_payload).unwrap())
+            .into_iter()
+            .rev()
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap();
+
+        // check deposit requirement, contract owner doesn't pay
+        let deposit = env::attached_deposit();
+        if !owner {
+            require!(deposit >= COST, "pay the piper");
+        }
+
+        // call mpc sign and return promise
+        mpc::ext(MPC_CONTRACT_ACCOUNT_ID.parse().unwrap())
+            .with_static_gas(Gas::from_tgas(100))
+            .sign(reversed_rlp_payload, path, key_version)
+    }
+}
